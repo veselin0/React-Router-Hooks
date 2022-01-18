1af26114843edec1c161297f8459de7516d458dc
Veselin <vesselin60@gmail.com>
2022-01-18 10:06:54 +0200

    react router useLocation

--- src/pages/services/ServiceDetail.js
+++ src/pages/services/ServiceDetail.js
@@ -1,27 +1,16 @@
 import {useParams, useHistory} from "react-router-dom"
 
-import servicesData from "./ServicesData"
+import servicesData from "./servicesData"
 
 function ServiceDetail() {
     const {serviceId} = useParams()
-    const history = useHistory()
     const thisService = servicesData.find(service => service._id === serviceId)
-
-    
-    function handleClick() {
-        console.log("Submitting...")
-        setTimeout(() => {
-            history.push("/services")
-        }, 2000)
-        history.replace("")
-    }
     
     return (
         <div>
             <h1>Service Detail Page</h1>
             <h3>{thisService.name} - ${thisService.price}</h3>
             <p>{thisService.description}</p>
-            <button onClick={handleClick}>Go back to all services</button>
         </div>
     )
 }
--- src/pages/services/ServicesList.js
+++ src/pages/services/ServicesList.js
@@ -1,8 +1,18 @@
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 
-import servicesData from "./ServicesData"
+import servicesData from "./servicesData"
 
 function ServicesList() {
+    const location = useLocation()
+    console.log(location)
+    
+// {
+//     pathname: "/services", 
+//     search: "?something=hello&blah=goodbye", 
+//     hash: "", 
+//     state: null, 
+//     key: "qyfp8w"
+// }
     const services = servicesData.map(service => (
         <div key={service._id}>
             <h3><Link to={`/services/${service._id}`}>{service.name}</Link> - ${service.price}</h3>
